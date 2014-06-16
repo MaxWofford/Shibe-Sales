60c3dc5b7e32cfb6f4bf7bf5235ade922a69b7fc
MaxWofford <max.speed.wofford@gmail.com>
2014-06-16 01:42:28 -0700

    Switched to layout header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "faq" => "pages#faq"
+  get "faq" => "pages#faq" #creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
