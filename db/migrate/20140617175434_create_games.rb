83e380db3fcefa1ac82c12c2120ca53084961d1b
MaxWofford <max.speed.wofford@gmail.com>
2014-06-25 13:40:33 -0700

    Fixed description length

--- db/migrate/20140617175434_create_games.rb
+++ db/migrate/20140617175434_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.string :reducedPrice
       t.integer :sale
 
