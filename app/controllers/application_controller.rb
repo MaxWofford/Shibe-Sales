59fd0c99057f06981c90b520f7f2bb6e662bc141
MaxWofford <max.speed.wofford@gmail.com>
2014-06-26 12:41:41 -0700

    Defined doge_api and doge_chain locally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #Sanity checking that 1 + 2 = 3
+  $cake = 1
+  $pie = 2
+  $spy = $cake + $pie
 end
