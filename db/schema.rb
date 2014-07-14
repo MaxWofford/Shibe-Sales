87ba4d8c010f46ce88b3ab2f2bd9e8fb583e6e51
MaxWofford <max.speed.wofford@gmail.com>
2014-07-13 23:41:25 -0700

    Added stock system

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709203106) do
+ActiveRecord::Schema.define(version: 20140712174516) do
 
   create_table "games", force: true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140709203106) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+    t.integer  "stock"
   end
 
   add_index "games", ["user_id"], name: "index_games_on_user_id"
